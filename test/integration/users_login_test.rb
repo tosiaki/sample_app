cf3b112785d2d46965161730ad90e6bf705e314a
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2018-06-08 20:34:37 -0400

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -33,4 +33,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "logout while not logged in" do
+    get root_path
+    assert_not is_logged_in?
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
 end
