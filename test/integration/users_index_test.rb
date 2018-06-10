a09b36be8b68c9d1d5e633b2910fbe9048183cce
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2018-06-10 01:51:39 -0400

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -12,7 +12,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
-    User.paginate(page: 1).each do |user|
+    User.where(activated: true).paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
@@ -27,5 +27,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "index excludes unactivated users" do
+    log_in_as(@admin)
+    pages = User.paginate(page: 1).total_pages
+    (1..pages).each do |n|
+      get users_path(page: n)
+      assert_select 'a', text: 'Inactive User', count: 0
+    end
   end
 end
